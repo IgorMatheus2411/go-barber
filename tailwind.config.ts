4738764035cfd86bb50dad2a2de3a9ce55e405d1
Igor <occon24@gmail.com>
2024-05-01 22:13:01 -0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+     colors: {
+      "background": "#312E38",
+      "gray": "#999591",
+      "orange": "#FF9000",
+      "inputs": "#232129",
+      "background-medium": "#28262E",
+      "shape": "#3E3B47",
+      "done": "#04D361",
+
+
+     }
+    },
+  },
+  plugins: [],
+};
+export default config;
